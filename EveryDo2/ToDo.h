94ac6e011f6e3a48395dbd9d109945b3ba9e9887
Shreve-La <shrevela@gmail.com>
2017-07-11 11:41:32 -0400

    initial commit

---
+++ EveryDo2/ToDo.h
@@ -0,0 +1,13 @@
+//
+//  ToDo.h
+//  EveryDo2
+//
+//  Created by swcl on 2017-07-11.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ToDo : NSObject
+
+@end
---
+++ EveryDo2/ToDo.m
@@ -0,0 +1,13 @@
+//
+//  ToDo.m
+//  EveryDo2
+//
+//  Created by swcl on 2017-07-11.
+//  Copyright © 2017 Shreve.ca. All rights reserved.
+//
+
+#import "ToDo.h"
+
+@implementation ToDo
+
+@end
